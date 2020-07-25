5445835ab4060ff4b83a2de5a3fe546d2b4a707d
PaoloCatalano <paolo12587@gmail.com>
2020-07-26 00:16:24 +0200

    popup

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,25 @@
-import React from 'react'
-import logo from '../assets/fido-logo-svg.svg'
+import React from "react"
+import logo from "../assets/fido-logo-svg.svg"
+import Links from "../constants/socialLinks"
 
 const Navbar = () => {
-    return (
-        <div className="navbar">
-            <div className="sfondo-logo">
-            <img src={logo} alt="logo" className="logo"/>
-            </div>
-            <h1>Le foto dei nostri cuccioli</h1>
-        </div>
-    )
+  //funzione POPUP
+  function myFunction() {
+    var popup = document.getElementById("myPopup")
+    popup.classList.toggle("show")
+  }
+
+  return (
+      <div className="navbar popup" onClick={myFunction}>
+      <div className="sfondo-logo">
+        <img src={logo} alt="logo" className="logo"/>
+      </div>
+      <h1>Le foto dei nostri cuccioli</h1>
+      <span className="popuptext" id="myPopup">
+        <Links />
+      </span>
+    </div>
+  )
 }
 
 export default Navbar
