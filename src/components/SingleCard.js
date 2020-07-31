2c7771012f56db213db64a79ae178ae74d7f5261
PaoloCatalano <paolo12587@gmail.com>
2020-07-31 07:59:36 +0200

    color

--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -7,9 +7,8 @@ const SingleCard = ({ nome, foto, contentful_id }) => {
     <Link to={`${contentful_id}`}>
       <div className="parent">
         <div className="child">
-          <div className="nome">{nome}</div>
-          <div className="underline"></div>
           <Image className="foto" fluid={foto[0].fluid} key={foto[0].id} />
+          <div className="nome">{nome}</div>
         </div>
       </div>
     </Link>
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,22 +5,6 @@ import Cards from "../components/Cards"
 import Overlay from "../components/Overlay"
 const Home = ({ data }) => {
 
-  
-  /* const [appear, setAppear] = React.useState(true)
-
-  const showPic = (nome) => {
-    console.log(nome);
-
-    for (var i = 0; i <= data.cani.nodes.length; i++){
-      if (data.cani.nodes[i].nome === nome) {
-        setAppear(false)
-      } else {
-        return null;
-      }
-    }
-    console.log(data.cani.nodes.length);
-  } */
-
   return (
     <Layout>
       <Cards cani={data.cani.nodes} ></Cards>
